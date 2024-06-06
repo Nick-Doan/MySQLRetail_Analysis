0ded50afacc82b5e5dd54caf5c7bc0f30251f1b0
Gooyam <ndoan@pdx.edu>
2024-06-05 21:23:29 -0700

    looked at top selling products

--- online_retailSQL.sql
+++ online_retailSQL.sql
@@ -6,5 +6,18 @@ FROM online_retail.retail_data
 GROUP BY Country;
 
 
-SELECT StockCode, Description, Quantity, UnitPrice, Country
+SELECT StockCode, Quantity UnitPrice, Country, Description
 FROM online_retail.retail_data
+
+-- Top selling products
+SELECT 
+    StockCode, 
+    TRIM(LOWER(Description)) AS normalized_description,
+    SUM(Quantity) AS total_quantity_sold
+FROM 
+    online_retail.retail_data
+GROUP BY 
+    StockCode, TRIM(LOWER(Description))
+ORDER BY 
+    total_quantity_sold DESC
+LIMIT 10;
